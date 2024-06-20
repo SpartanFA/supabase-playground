67aeb747a853998401cbfc7be404252debaa3b38
Ben <bewerner23@gmail.com>
2024-06-19 21:41:25 -0400

    changing to batching multiple users at once

--- app/api/batch-create-clerk-user/route.ts
+++ app/api/batch-create-clerk-user/route.ts
@@ -2,28 +2,33 @@ import { inngest } from "@/app/inngest/inngest.client";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
-const schema = z.object({
-  email: z.string(),
-  hashed_password: z.string(),
-  external_id: z.string(),
-});
+const schema = z.array(
+  z.object({
+    email: z.string(),
+    hashed_password: z.string(),
+    external_id: z.string(),
+  })
+);
 
 export const POST = async (req: NextRequest, res: NextResponse) => {
   if (!req.body) {
     return NextResponse.json({ message: "No body" }, { status: 400 });
   }
-  const { email, hashed_password, external_id } = schema.parse(
-    await req.json()
+  const users = schema.parse(await req.json());
+
+  await Promise.all(
+    users.map((user) => {
+      const { email, hashed_password, external_id } = user;
+      return inngest.send({
+        name: "migration/batch/create-clerk-user",
+        data: {
+          email,
+          hashed_password,
+          external_id,
+        },
+      });
+    })
   );
 
-  await inngest.send({
-    name: "migration/batch/create-clerk-user",
-    data: {
-      email,
-      hashed_password,
-      external_id,
-    },
-  });
-
-  return NextResponse.json({ message: "User queued" });
+  return NextResponse.json({ message: `${users.length} users queued` });
 };
