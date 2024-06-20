dd4abeb8a505a93bee6d363d97832ff7b0eba867
Ben <bewerner23@gmail.com>
2024-06-20 14:31:45 -0400

    making create user an upsert

--- app/inngest/functions/create-clerk-user.ts
+++ app/inngest/functions/create-clerk-user.ts
@@ -1,4 +1,5 @@
 import { clerkClient } from "@clerk/clerk-sdk-node";
+import type { User } from "@clerk/clerk-sdk-node";
 import { inngest } from "../inngest.client";
 export const trickleCreateClerkUser = inngest.createFunction(
   {
@@ -17,6 +18,16 @@ export const trickleCreateClerkUser = inngest.createFunction(
   async ({ event, step }) => {
     const { email, hashed_password, external_id } = event.data;
 
+    const { editedUser } = await updatePotentialUser(
+      external_id,
+      email,
+      hashed_password
+    );
+
+    if (editedUser) {
+      return JSON.stringify({ editedUser });
+    }
+
     const newUser = await clerkClient.users.createUser({
       externalId: external_id,
       emailAddress: [email],
@@ -29,3 +40,41 @@ export const trickleCreateClerkUser = inngest.createFunction(
     return JSON.stringify(newUser);
   }
 );
+
+async function updatePotentialUser(
+  external_id: string,
+  email: string,
+  hashed_password: string
+) {
+  const findUser = await clerkClient.users.getUserList({
+    externalId: [external_id],
+  });
+
+  if (findUser.totalCount >= 2) {
+    throw new Error("There are more than 2 users with the same external_id");
+  }
+
+  if (findUser.totalCount === 1) {
+    const existingUser = findUser.data[0];
+
+    if (
+      !existingUser.emailAddresses.some(
+        (emailObj) => emailObj.emailAddress === email
+      )
+    ) {
+      throw new Error(
+        "The provided email does not match any existing user emails, and we cannot modify the clerk email address"
+      );
+    }
+
+    const editedUser = await clerkClient.users.updateUser(existingUser.id, {
+      passwordDigest: hashed_password,
+      passwordHasher: "bcrypt",
+      skipPasswordChecks: true,
+    });
+
+    return { editedUser };
+  }
+
+  return { editedUser: undefined };
+}
